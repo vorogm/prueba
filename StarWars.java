960e658e13ec074ffc000f4e69f0faab581e021f
prueba <prueba@prueba.es>
2024-11-22 10:56:28 +0100

    Cambio StarWars

--- StarWars.java
+++ StarWars.java
@@ -1,5 +1,5 @@
 public class Ejemplo {
       public static void main(String[ ] arg) {
-      System.out.println("Hola Mundo maravilloso");
+      System.out.println("QUE LA FUERZA TE ACOMPAÑE");
       }
 }
