48d3252a54c0a7c188a23d71e8b6b6c763e3aac7
Salvador <sagrescsv@gmail.com>
2024-11-23 10:57:35 +0100

    Modificaciones desde linux

--- StarWars.java
+++ StarWars.java
@@ -1,5 +1,6 @@
-public class Ejemplo {
+public class StarWars {
       public static void main(String[ ] arg) {
       System.out.println("QUE LA FUERZA TE ACOMPAÑE");
+	System.out.println("modificado en linux 23-11-2024");
       }
 }
